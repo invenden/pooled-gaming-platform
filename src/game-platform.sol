030ac56160af91714abbe7cc4756a3ab5594d208
Will Hausman <will.jhausman@outlook.com>
2020-10-14 01:21:32 -0500

    all syntax errors gone.

--- src/game-platform.sol
+++ src/game-platform.sol
@@ -11,13 +11,14 @@ interface TRC20 {
 }
 
 interface GameContract {
+    function minimumDeposit() external view returns (uint256 minimum);
     function tokenAddress() external view returns (address);
     function deposit(uint256 amount) external returns (bool success);
     function withdraw() external returns (uint256 amount);
     function claim() external returns (uint256 amount);
 }
 
-contract Divpool {
+contract PooledGamePlatform {
     using SafeMath for uint256;
 
     struct Player {
@@ -27,9 +28,10 @@ contract Divpool {
 
     struct Game {
         address gameAddress;
-        address trc20Address;
+        address tokenAddress;
         uint gameEndsOn;
         mapping(address=>Player) players;
+        address[] _players;
     }
 
     address internal dev;
@@ -44,102 +46,127 @@ contract Divpool {
     }
 
     //// PLAYER FUNCTIONS ////
-    function deposit(address tokenAddress, uint amount, address referredBy) onlyDuringGame(tokenAddress) payable public returns (uint) { // TODO: msg.value and trc tokens...
-        require(msg.value > 0, "Cannot purchase 0 tokens.");
+    function deposit(address tokenAddress, uint amount, address referredBy) onlyDuringGame(tokenAddress) public returns (uint) { // TODO: msg.value and trc tokens...
+        require(amount > 0, "Cannot purchase 0 tokens.");
         Game storage game = games[tokenAddress];
         Player storage player = game.players[msg.sender];
         if (player.referredBy == address(0)) { // new player
             player.referredBy = referredBy != address(0) ? referredBy : dev;
         }
 
-        uint netAmount = extractFees(msg.value, true);
+        uint netAmount = extractFees(game, amount, true);
         player.shares = player.shares.add(netAmount);
         return player.shares;
     }
 
-    function withdraw() onlyActivePlayers() onlyDuringGame() public returns (uint) {
-        Player storage player = players[msg.sender];
+    function withdraw(address tokenAddress) onlyDuringGame(tokenAddress) onlyActivePlayers(tokenAddress) public returns (uint) {
+        Game storage game = games[tokenAddress];
+        Player storage player = game.players[msg.sender];
         uint shares = player.shares;
-        uint toWithdraw = extractFees(shares, false);
-        msg.sender.transfer(toWithdraw); // TODO: Send correct token type.
+        uint toWithdraw = extractFees(game, shares, false);
+        TRC20 trc20 = TRC20(tokenAddress);
+        trc20.transfer(msg.sender, toWithdraw);
         player.shares = 0;
         return toWithdraw;
     }
 
-    function claim() onlyActivePlayers() onlyBetweenGames() public returns (uint) {
-        return _claim(msg.sender);
+    function claim(address tokenAddress) onlyBetweenGames(tokenAddress) onlyActivePlayers(tokenAddress) public returns (uint) {
+        return _claim(tokenAddress, msg.sender);
     }
 
-    function _claim(address payable receiver) internal returns (uint) {
-        Player storage player = players[receiver];
+    function _claim(address tokenAddress, address receiver) internal returns (uint) {
+        Player storage player = games[tokenAddress].players[receiver];
         uint shares = player.shares;
-        receiver.transfer(shares); // TODO: Send correct token type.
+        TRC20 trc20 = TRC20(tokenAddress);
+        trc20.transfer(receiver, shares);
         player.shares = 0;
         return shares;
     }
 
-    function claimableShares() public view returns (uint) {
-        return players[msg.sender].shares;
+    function claimableShares(address tokenAddress) public view returns (uint) {
+        return games[tokenAddress].players[msg.sender].shares;
     }
 
-    function sellableShares() public view returns (uint) {
-        return players[msg.sender].shares.percentage(90); // total 10% fee will be applied if player sells.
+    function sellableShares(address tokenAddress) public view returns (uint) {
+        return games[tokenAddress].players[msg.sender].shares.percentage(90); // total 10% fee will be applied if player sells.
     }
 
-    function addToGame(address gameAddress, uint amount) onlyDuringGame(gameAddress) public {
-        require(amount > 0, "Cannot add 0 tokens to the game.");
-
-        poolContract.transfer(amount); // TODO: actually call the right method for staking.
+    function addToGame(address tokenAddress, uint amount) onlyDuringGame(tokenAddress) public {
+        GameContract gameContract = GameContract(games[tokenAddress].gameAddress);
+        require(amount > gameContract.minimumDeposit(), "Insufficient tokens to add.");
+        gameContract.deposit(amount); // TODO: make sure this call works
     }
 
     //// ADMIN FUNCTIONS ////
-    function startGame(address gameAddress, uint daysToRun) onlyAdmins() onlyBetweenGames(gameAddress) public {
-        Game storage game = games[gameAddress];
+    function startGame(address tokenAddress, uint daysToRun) onlyAdmins() onlyBetweenGames(tokenAddress) public {
+        Game storage game = games[tokenAddress];
         require(block.timestamp >= game.gameEndsOn + WAITING_PERIOD, "A new game cannot begin until players have had enough time to collect their earnings from the last game.");
         require(daysToRun > 0, "Cannot run a game for 0 days.");
         game.gameEndsOn = block.timestamp.add(daysToRun.mul(SECONDS_PER_DAY));
+        
+        GameContract gameContract = GameContract(game.gameAddress);
+        TRC20 trc20 = TRC20(game.tokenAddress);
+        uint seed = trc20.balanceOf(address(this));
 
-        // add seed balance to contract balance, stake entire balance to stakingContract.
-        // set all players' balances (if any remain) to 0
+        if (seed >= gameContract.minimumDeposit()) {
+            gameContract.deposit(seed);
+        }
+
+        for (uint i=0; i<game._players.length; i++) {
+            game.players[game._players[i]].shares = 0;
+        }
     }
-    function endGame() onlyAdmins() onlyDuringGame() public {
-        gameStarted = false;
-        gameCanStartOn = block.timestamp + waitingPeriod;
+
+    function endGame(address tokenAddress) onlyAdmins() onlyDuringGame(tokenAddress) public {
+        Game storage game = games[tokenAddress];
+        game.gameEndsOn = block.timestamp; // TODO: onlyDuringGame() will fail here... really truly absolutely must use gameStarted bool
 
         // Dev is always a player, but never a winner.
-        _claim(dev);
+        _claim(tokenAddress, dev);
 
-        // unstake from stakingContract
-        // calculate distributions
-        // add to the players
+        GameContract gameContract = GameContract(game.gameAddress);
+        uint claimedRewards = gameContract.claim();
+
+        TRC20 trc20 = TRC20(game.tokenAddress);
+        uint balance = trc20.balanceOf(address(this));
+        for (uint i=0; i< game._players.length; i++) {
+            Player storage player = game.players[game._players[i]];
+            uint newShares = player.shares.div(balance).mul(claimedRewards);
+            player.shares = player.shares.add(newShares);
+        }
     }
+
     function registerAdmin(address addr, bool isAdmin) onlyAdmins() public {
         require(addr != dev, "The dev will always be an Administrator.");
         admin[addr] = isAdmin;
     }
 
     function registerGame(address gameAddress) onlyAdmins() public {
-        Game storage game = games[gameAddress];
-        require(game.gameAddress == address(0), "The game cannot be altered once it has been registered.");
         GameContract gameContract = GameContract(gameAddress);
+        address tokenAddress = gameContract.tokenAddress();
+        Game storage game = games[tokenAddress];
         
+        require(game.gameEndsOn < block.timestamp, "A game is currently running.");
+        game.gameAddress = gameAddress;
+        game.tokenAddress = tokenAddress;
+        // leave players alone, so they can still claim any possible rewards if we're changing gameAddresses on a token.
     }
 
     //// INTERNAL ////
-    function extractFees(uint amount, bool shouldPayReferrer) internal returns (uint) {
+    function extractFees(Game storage game, uint amount, bool shouldPayReferrer) internal returns (uint) {
         uint toPool = amount.percentage(4);
         uint toDivs = amount.percentage(4);
         uint toReferral = amount.percentage(2);
 
         if (shouldPayReferrer) {
-            payReferral(players[msg.sender].referredBy, toReferral);
+            payReferral(game, game.players[msg.sender].referredBy, toReferral);
         } else {
             toPool = toPool.add(toReferral);
             toReferral = 0;
         }
 
-        payDividends(msg.sender, toDivs);
-        payPool(toPool);
+        payDividends(game, msg.sender, toDivs);
+        payPool(game, toPool);
 
 
         uint netAmount = amount.sub(toPool).sub(toDivs).sub(toReferral);
@@ -147,30 +174,31 @@ contract Divpool {
         return netAmount;
     }
 
-    function payDividends(address exclude, uint dividends) internal {
+    function payDividends(Game storage game, address exclude, uint dividends) internal {
         // TODO
     }
 
-    function payPool(uint amount) internal {
+    function payPool(Game storage game, uint amount) internal {
         // TODO
         // hold amount in ledger. if ledger > threshold for poolContract, send. (50 for opals staking)
+        addToGame(game.tokenAddress, amount);
     }
 
-    function payReferral(address referrer, uint amount) internal {
-        if (players[referrer].shares > 0) {
-            payPlayer(referrer, amount);
+    function payReferral(Game storage game, address referrer, uint amount) internal {
+        if (game.players[referrer].shares > 0) {
+            payPlayer(game, referrer, amount);
         } else {
-            payPlayer(dev, amount);
+            payPlayer(game, dev, amount);
         }
     }
 
-    function payPlayer(address player, uint amount) internal {
-        players[player].shares = players[player].shares.add(amount);
+    function payPlayer(Game storage game, address player, uint amount) internal {
+        game.players[player].shares = game.players[player].shares.add(amount);
     }
 
     //// MODIFIERS ////
-    modifier onlyActivePlayers() {
-        require(players[msg.sender].shares > 0, "You are not eligible.");
+    modifier onlyActivePlayers(address tokenAddress) {
+        require(games[tokenAddress].players[msg.sender].shares > 0, "You are not eligible.");
         _;
     }
 
@@ -179,13 +207,13 @@ contract Divpool {
         _;
     }
 
-    modifier onlyDuringGame(address gameAddress) {
-        require(games[gameAddress].gameEndsOn > block.timestamp, "A game is not running.");
+    modifier onlyDuringGame(address tokenAddress) {
+        require(games[tokenAddress].gameEndsOn > block.timestamp, "A game is not running.");
         _;
     }
 
-    modifier onlyBetweenGames(address gameAddress) {
-        require(games[gameAddress].gameEndsOn <= block.timestamp, "A game is currently running.");
+    modifier onlyBetweenGames(address tokenAddress) {
+        require(games[tokenAddress].gameEndsOn <= block.timestamp, "A game is currently running.");
         _;
     }
 }
